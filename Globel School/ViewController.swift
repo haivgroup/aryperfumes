65c118970eb2afea5d07451715b8c5f0dcf2edef
Haiv Group <haivgroup@hmail.com>
2023-07-07 17:24:19 +0300

    Initial Commit

---
+++ Globel School/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Globel School
+//
+//  Created by Muhammad on 7/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
