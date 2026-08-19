610f7e1cc88195ed9517944344080cef6be4e9fb
test <muhammad@Muhammads-MacBook-Pro.local>
2026-08-19 16:05:48 -0700

    Initial Xcode project upload

---
+++ Globel School/NetworkErrorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkErrorViewController.swift
+//  Globel School
+//
+//  Created by Muhammad on 7/7/23.
+//
+
+import UIKit
+
+class NetworkErrorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
